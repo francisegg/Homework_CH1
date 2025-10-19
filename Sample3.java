d8606577152fa8d350638cdda92ba91cb54e7a05
tea-egg-tw <bm6hjj2020.tw@gmail.com>
2025-10-19 23:35:27 +0800

    Feat: Sample3

---
+++ Sample3.java
@@ -0,0 +1,11 @@
+//Chapter1 p.16
+public class Sample3
+{
+    public static void main(String[] args) 
+    {
+        System.out.println('A');                //字元常數
+        System.out.println("歡迎使用Java!");    //字串常數
+        System.out.println(123);               //整數常數、浮點數常數
+    }
+    
+}
