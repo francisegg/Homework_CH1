3c4690a122d0d3a85acaee6dec9e32694a378b7d
tea-egg-tw <bm6hjj2020.tw@gmail.com>
2025-10-19 23:21:11 +0800

    Feat: Sample1
    - Chapter1 p.9
    - add code and result capture

---
+++ Sample1.java
@@ -0,0 +1,9 @@
+//Chapter1 p.9
+public class Sample1
+{
+    public static void main(String[] args)
+    {
+        System.out.print("歡迎使用Java!");
+        System.out.println("開始使用Java吧!");
+    }
+}
