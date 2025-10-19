68d14d331dc4849d38128f275ef73e97b4f0b2f0
tea-egg-tw <bm6hjj2020.tw@gmail.com>
2025-10-19 23:41:01 +0800

    Feat: 跳脫字元
    - Chepter1 p.18
    -Sample4 and Sample5

---
+++ Sample4.java
@@ -0,0 +1,11 @@
+//Chapter1 p.18
+//跳脫字元
+public class Sample4
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("顯示出反斜線:\\"); 
+        System.out.println("顯示出單引號\'");
+    }
+    
+}
---
+++ Sample5.java
@@ -0,0 +1,11 @@
+//Chapter1 p.18
+//跳脫字元
+public class Sample5
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("八進101的字元是\101"); 
+        System.out.println("十六進位數0061的字元是\u0061");
+    }
+    
+}
