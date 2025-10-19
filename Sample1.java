9b65e3bdd4f5ec7e8ceb0594f35c5647c4f77c4f
tea-egg-tw <bm6hjj2020.tw@gmail.com>
2025-10-19 23:24:10 +0800

    Fix: 修正 Sample1語法錯誤

--- Sample1.java
+++ Sample1.java
@@ -4,6 +4,6 @@ public class Sample1
     public static void main(String[] args)
     {
         System.out.print("歡迎使用Java!");
-        System.out.println("開始使用Java吧!");
+        System.out.print("開始使用Java吧!");
     }
 }
