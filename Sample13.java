f2cb1a1652de7c95838b5ca1052818d6ff42ac50
tea-egg-tw <bm6hjj2020.tw@gmail.com>
2025-10-21 22:17:05 +0800

    Feat: Sample13 p.
    - Chapter1.4 p.38
    - 資料型態的轉換

---
+++ Sample13.java
@@ -0,0 +1,17 @@
+//Chapter1.4 p.38
+//資料型態的轉換
+public class Sample13 
+{
+    public static void main(String[] args) 
+    {
+        double dnum = 160.5;
+
+        System.out.println("身高是"+dnum+"公分");
+
+        System.out.println("指定給int型態的變數");
+
+        int inum = (int) dnum;
+
+        System.out.println("身高是"+inum+"公分");
+    }
+}
